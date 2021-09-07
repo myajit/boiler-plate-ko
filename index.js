b6a2f0e8a274c316c18037fd5b073a995257b614
myajit <myajit@naver.com>
2021-09-07 14:15:26 +0900

    second

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
+const { User } = require("./models/User");
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://jung:1234@cluster0.bh6gf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser:true, useUnifiedTopology:true
 }) .then (() => console.log('mongoDB Connected'))
 .catch(err => console.log(err))
 
-
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+//aplication/json
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.post('/register',(req,res)=> {
+  
+  //회원가입 정보를 client에서 가져오면
+  //그것들을 데이터 베이스에 넣어준다.
+
+
+  const user = new User(req.body);
+
+  user.save((err,userInfo) => {
+    if(err) return res.json({success: false,err})
+    return res.status(200).json({
+      success: true
+    })
+  });
+
+})
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
